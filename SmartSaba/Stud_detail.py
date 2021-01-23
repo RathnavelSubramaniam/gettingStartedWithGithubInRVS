3228d0db7ff7993927cf67495f6aa8fd4d3854f4
SmartSaba4 <77879315+SmartSaba4@users.noreply.github.com>
2021-01-23 16:10:54 +0530

    Sabarish Ramasamy

---
+++ SmartSaba/Stud_detail.py
@@ -0,0 +1,20 @@
+#getting Started With Gethub
+#RVS CAS
+class Student:
+    def __init__(self):
+        self.name="Sabarish"
+s=Student()
+
+print("My name is ",s.name)
+class Skills:
+    def __init__(self):
+        self.Skill_1="python language"
+        self.Skill_2="Java language"
+        self.Skill_3="html5 & CSS"
+a=Skills()
+print("My Skills : ")
+print("1.",a.Skill_1)
+print("2.",a.Skill_2)
+print("3.",a.Skill_3)
+
+
